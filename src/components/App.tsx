fb1a3b796355243a40c0d573e41aead620a8b5a4
Hairum Qureshi <hqureshi@udel.edu>
2025-02-11 20:25:59 -0500

    added styling to the footer, another bullet, and updated footer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -135,27 +135,19 @@ export default function App() {
 						<div className="font-secondary text-lg sm:text-2xl">
 							<div className="flex flex-col sm:flex-row justify-between border-b pb-2">
 								<h3>20 piece potato samosas</h3>
-								<h3 className="text-orange-500 font-semibold">
-									$4.99
-								</h3>
+								<h3 className="text-orange-500 font-semibold">$4.99</h3>
 							</div>
 							<div className="flex flex-col sm:flex-row justify-between border-b py-2">
 								<h3>20 piece chicken and vegetable samosas</h3>
-								<h3 className="text-orange-500 font-semibold">
-									$8.99
-								</h3>
+								<h3 className="text-orange-500 font-semibold">$8.99</h3>
 							</div>
 							<div className="flex flex-col sm:flex-row justify-between border-b py-2">
 								<h3>20 piece chicken and vegetable rolls</h3>
-								<h3 className="text-orange-500 font-semibold">
-									$10.49
-								</h3>
+								<h3 className="text-orange-500 font-semibold">$10.49</h3>
 							</div>
 							<div className="flex flex-col sm:flex-row justify-between border-b py-2">
 								<h3>20 piece potato rolls</h3>
-								<h3 className="text-orange-500 font-semibold">
-									$5.49
-								</h3>
+								<h3 className="text-orange-500 font-semibold">$5.49</h3>
 							</div>
 						</div>
 						<div className="text-center font-secondary text-base sm:text-xl my-5 text-green-500">
@@ -200,7 +192,13 @@ export default function App() {
 							<p>6. All products are 100% halal certified</p>
 							<br />
 							<p>
-								7. Currently, we only deliver within an 8-mile radius of Newark, DE, including areas like Bear, Glasgow, Christiana, and parts of Middletown.
+								7. Currently, we only deliver within an 7-mile radius of Newark,
+								DE, including areas like Bear, Glasgow, Christiana, and parts of
+								Middletown.
+							</p>
+							<br />
+							<p>
+								8. If you live 8-10 miles and are interested in ordering, there will be an extra cost of $4.63 for delivery charges
 							</p>
 						</div>
 					</div>
@@ -221,14 +219,20 @@ export default function App() {
 						</div>
 					</div>
 				</div> */}
-				<footer className="w-full p-2 mt-14 bg-sky-950 text-center">
+				<footer className="relative w-full p-2 mt-14 bg-sky-950 text-center border-t-2 border-t-sky-500">
+					{/* Sky Blue Glow at the Top */}
+					<div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full h-20 bg-gradient-to-b from-sky-400 to-transparent blur-3xl opacity-60"></div>
+
 					<h3 className="text-5xl text-sky-400 font-tertiary mt-5">
 						Hungry? Call (or text) Mohid at (302)-344-7160 <br /> or Hairum at
 						(302)-480-0559
 					</h3>
 					<h3 className="text-3xl text-sky-300 font-tertiary">
-						WE OFFER FREE DELIVERY!
+						WE OFFER FREE DELIVERY!*
 					</h3>
+					<p className = "text-xs font-secondary">
+						Unless you live outside the 7 mile radius, additional delivery charges will apply. See disclaimer #8 for more details*	
+					</p>
 					<p className="text-white text-base mt-5">
 						Copyright {new Date().getFullYear()} All Rights Reserved
 					</p>
